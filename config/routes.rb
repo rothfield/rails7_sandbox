d966a62e9b40f55375eeda0d2bc37b47fdbb4e13
John Rothfield <johnrothfield@gmail.com>
2022-01-28 01:18:20 +0000

    Added Inspections, Customers,Machines,Tasks,Logs and Invoices as links in Navbar, Added corresponding controllers, views, and routes Added logo as rails asset and linked to it in Navbar Links to profile, settings, and logout set to "#"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'invoices/index'
+  get 'logs/index'
+  get 'tasks/index'
+  get 'machines/index'
+  get 'customers/index'
+  get 'inspections/index'
   get 'home/index'
 
   root "home#index"
