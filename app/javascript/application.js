49c4b0c82f7b854ae05e1c755857198668fe6672
John Rothfield <johnrothfield@gmail.com>
2022-02-02 16:34:44 +0000

    Alpine.start() necessary for navbar!!

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import Alpine from "alpinejs"
 window.Alpine=Alpine
+Alpine.start()
 import "@hotwired/turbo-rails"
 import { Turbo } from "@hotwired/turbo-rails"
 window.Turbo= Turbo
