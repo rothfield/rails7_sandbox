3d5258c262274ce1772e7df466de4401f0687fe8
John Rothfield <johnrothfield@gmail.com>
2022-02-13 08:50:18 -0800

    dropdown tweaks

--- app/javascript/controllers/components/README.md
+++ app/javascript/controllers/components/README.md
@@ -2,3 +2,9 @@ Put component controllers here. For example,
 for app/components/dropdown, use:
 
 app/javascripts/controllers/components/dropdown_controller.js
+
+And in the html:
+
+data-controller="components--dropdown"
+data-components--dropdown-target="menu"
+
--- app/javascript/controllers/components/dropdown_controller.js
+++ app/javascript/controllers/components/dropdown_controller.js
@@ -1,6 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+
+  static targets = ["menu"]
+
+  toggleMenu() {
+    console.log("Hello, toggleMenu!", this.element);
+     this.menuTarget.classList.toggle('hidden');
+  }
+
   connect() {
     console.log("Hello, Stimulus!", this.element);
   }
